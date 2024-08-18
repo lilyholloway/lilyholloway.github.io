557312f8f0208c90c54c2925020a643629eeda46
Zephyr Zhang <lily.holloway@gmail.com>
2024-08-19 03:43:35 +1200

    Update GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig, {
-    images: {
-      unoptimized: true,
-    },
-    assetPrefix: '.',
-  }
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
