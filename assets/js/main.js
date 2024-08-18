ec5f5091235b97861d07956806b71a9cd0b309ba
Zephyr Zhang <lily.holloway@gmail.com>
2024-08-18 19:36:49 +1200

    Delete extra files

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// src/main.ts
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Website loaded');
-    // Add any custom TypeScript code here
-});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/tachyons@4.6.1/css/tachyons.min.css" />
+        <title>Lily Holloway</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
